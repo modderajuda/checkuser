37e9f2c17d432d5274306dc5ea86de1bbd03d5cb
modderajuda <36990836+modderajuda@users.noreply.github.com>
2023-10-24 19:08:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ azul="\e[34m"
 roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
-rm -rf /root/modderajuda/
+rm -rf /root/checkuser/
 rm -f /usr/local/bin/iniciar
 sudo kill -9 $(lsof -t -i:5454)
-pkill -9 -f "/root/modderajuda/checkuser.py"
+pkill -9 -f "/root/checkuser/checkuser.py"
 
 
 apt update && apt upgrade -y && apt install python3 git -y
 git clone https://github.com/modderajuda/checkuser.git
-chmod +x /root/modderajuda/checkuserMenu.sh
-ln -s /root/modderajuda/checkuserMenu.sh /usr/local/bin/iniciar
+chmod +x /root/checkuser/checkuserMenu.sh
+ln -s /root/checkuser/checkuserMenu.sh /usr/local/bin/iniciar
 
 clear
 echo -e "Para iniciar o menu digite: ${verde}iniciar${reset}"
