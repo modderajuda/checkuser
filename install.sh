86ab185182df3030b456798340da7ee588068358
modderajuda <36990836+modderajuda@users.noreply.github.com>
2024-08-08 13:49:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,25 @@ azul="\e[34m"
 roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
+echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[38;5;118m        ⇱ BEM VINDO AO SCRIPT CHECKUSER DO MODDER!! ⇲             "
+echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo -e "             \033[1;31mATENCAO! \033[1;37mESSE SCRIPT IRA !\033[0m"
+echo ""
+echo -e "\033[1;31m• \033[1;37mATUALIZAR O SISTEMA DA MÁQUINA\033[0m"
+echo -e "\033[1;37m  ANTES DE INICIAR A INSTALAÇÃO DO CHECKUSER\033[0m"
+echo ""
+echo -e "\033[1;32m• \033[1;32mDICA! \033[1;37mULTILIZE O TEMA DARK EM SEU TERMINAL PARA\033[0m"
+echo -e "\033[1;37m  UMA MELHOR EXPERIÊNCIA E VISUALIZACAO DO MESMO!\033[0m"
+echo ""
+echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "            \033[1;37m • \033[1;32mEDIT:@donomodderajuda\033[1;37m •           "
+echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+#-----------------------------------------------------------------------------------------------------------------
+echo -ne "\033[38;5;118mENTER \033[1;37mpara continuar a \033[1;31mINSTALACAO : \033[0m"; read x
+[[ $x = @(n|N) ]] && exit
 rm -rf /root/checkuser/
 rm -f /usr/local/bin/iniciar
 sudo kill -9 $(lsof -t -i:5454)
@@ -19,3 +38,4 @@ ln -s /root/checkuser/checkuserMenu.sh /usr/local/bin/iniciar
 
 clear
 echo -e "Para iniciar o menu digite: ${amarelo}iniciar${reset}"
+#-----------------------------------------------------------------------------------------------------------------
